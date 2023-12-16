1478042e44479052b6eb9dee342ae7aa26e1fcfe
gevorah <jhonsebastianijajiortiz@gmail.com>
2023-12-16 13:55:53 -0500

    fix: day 4 part 1

--- Day 4/4.py
+++ Day 4/4.py
@@ -1,17 +1,19 @@
 import re
 
-def scratchcard(text: str):
+def scratchcard(card: str):
   split_regex = r'(?:\d+\s+)+\d+|\d+'
-  card, wins, nums = re.findall(split_regex, text)
+  card, wins, nums = re.findall(split_regex, card)
 
   find_regex = r'\b(?:' + re.sub('\s+', '|', wins) + r')\b'
   matches = re.findall(find_regex, nums)
 
-  return int(2 ** (len(matches) - 1))
+  return len(matches)
 
-with open('Day 4/input', 'r') as f:
+def points(file):
   result = 0
-  for line in f.readlines():
-    result += scratchcard(line)
-  
-  print(result)
+  for line in file.readlines():
+    result += int(2 ** (scratchcard(line) - 1))
+  return result
+
+with open('Day 4/test', 'r') as f:
+  print(points(f))
