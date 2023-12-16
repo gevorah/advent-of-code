175a0780634805eae895bdd32aafcdb25ae7959b
gevorah <jhonsebastianijajiortiz@gmail.com>
2023-12-15 21:09:33 -0500

    feat: day 3 part 2

--- Day 3/3.py
+++ Day 3/3.py
@@ -1,15 +1,16 @@
 import re
+from functools import reduce
 
 def engine(text: str):
   cols = text.find('\n') + 1
   result = 0
   for value in re.finditer(r'\d+', text):
     left = value.start() - 1
-    if left < 0 or text[left] == '\n': # \n1.+
+    if left < 0 or text[left] == '\n':
       left += 1
 
     right = value.end()
-    if right >= len(text) or text[right] == '\n': # +.1\n
+    if right >= len(text) or text[right] == '\n':
       right -= 1
 
     top = list(range(left - cols, right - cols + 1))
@@ -24,7 +25,32 @@ def engine(text: str):
 
   return result
 
+def engineEnhanced(text: str):
+  cols = text.find('\n') + 1
+  result = 0
+  for symbol in re.finditer(r'\*', text):
+    left = symbol.start() - 1
+    if left < 0 or text[left] == '\n':
+      left += 1
+
+    right = symbol.end()
+    if right >= len(text) or text[right] == '\n':
+      right -= 1
+
+    top = list(range(left - cols, right - cols + 1))
+    bottom = list(range(left + cols, right + cols + 1))
+
+    search = top + bottom + [left, right]
+
+    gear = []
+    for value in re.finditer(r'\d+', text):
+      if value.start() in search or value.end() - 1 in search:
+        gear.append(int(value.group()))
+
+    result += reduce((lambda x, y: x*y), gear) if len(gear) == 2 else 0
+
+  return result
 
 with open('Day 3/input', 'r') as f:
   text = ''.join(f.readlines())
-  print(engine(text))
+  print(engineEnhanced(text))
