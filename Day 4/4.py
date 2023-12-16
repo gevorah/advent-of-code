9e269ce266e99b3cf8ac87e0ee90455d36131211
gevorah <jhonsebastianijajiortiz@gmail.com>
2023-12-16 17:18:50 -0500

    feat: day 4 part 2

--- Day 4/4.py
+++ Day 4/4.py
@@ -2,18 +2,31 @@ import re
 
 def scratchcard(card: str):
   split_regex = r'(?:\d+\s+)+\d+|\d+'
-  card, wins, nums = re.findall(split_regex, card)
+  _id, wins, nums = re.findall(split_regex, card)
 
   find_regex = r'\b(?:' + re.sub('\s+', '|', wins) + r')\b'
-  matches = re.findall(find_regex, nums)
+  matches = len(re.findall(find_regex, nums))
 
-  return len(matches)
+  return matches
 
-def points(file):
+def points(cards: list[str]):
   result = 0
-  for line in file.readlines():
-    result += int(2 ** (scratchcard(line) - 1))
+  for card in cards:
+    result += int(2 ** (scratchcard(card) - 1))
   return result
 
-with open('Day 4/test', 'r') as f:
-  print(points(f))
+def n_copies(cards: list[str]):
+  copies = {x: 1 for x in range(len(cards))}
+
+  for index, card in enumerate(cards):
+    matches = scratchcard(card)
+    
+    for i in range(index + 1, index + matches + 1):
+      copies[i] += copies[index]
+  
+  return sum(copies.values())
+
+with open('Day 4/input', 'r') as f:
+  cards = f.readlines()
+  print(points(cards))
+  print(n_copies(cards))
