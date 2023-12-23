3cfcb9c7859be30604cf1f9773b696619e52a744
gevorah <jhonsebastianijajiortiz@gmail.com>
2023-12-23 12:27:40 -0500

    feat: day 5 part 2

--- Day 5/5.py
+++ Day 5/5.py
@@ -1,12 +1,6 @@
 import re
 from functools import reduce
-
-def compute(seed, ranges):
-  for values in ranges.split('\n'):
-    dst, src, i = map(int, re.findall(r'\d+', values))
-    if src <= seed < src + i:
-      return dst + (seed - src)
-  return seed
+import sys
 
 def decipher(almanac: str):
   split_regex = r'(?:\d+\s)+\d+'
@@ -14,12 +8,17 @@ def decipher(almanac: str):
 
   seeds = list(map(int, re.findall(r'\d+', seeds)))
 
-  return min(reduce(compute, maps, int(seed)) for seed in seeds)
+  return min(computeLoc(int(seed), maps) for seed in seeds)
 
-def computeEnhanced(seed, ranges):
-  start, length = seed
+def computeLoc(seed, maps):
+  return reduce(computeDst, maps, seed)
 
-  return [1, 1]
+def computeDst(seed, ranges):
+  for values in ranges.split('\n'):
+    dst, src, length = map(int, re.findall(r'\d+', values))
+    if src <= seed < src + length:
+      return dst + (seed - src)
+  return seed
 
 def decipherEnhanced(almanac: str):
   split_regex = r'(?:\d+\s)+\d+'
@@ -27,10 +26,40 @@ def decipherEnhanced(almanac: str):
 
   seeds = re.findall(r'\d+\s\d+', seeds)
   
-  return min(reduce(computeEnhanced, maps, list(map(int, seed.split())))[0] for seed in seeds)
+  locations = []
+  for seed in seeds:
+    start, length = map(int, re.findall(r'\d+', seed))
+    locations.append(quickSearch(start, length, maps))
+  
+  return min(locations)
 
+def quickSearch(start, length, maps):
+  queue = [(start, length)]
+  minLoc = sys.maxsize
 
-with open('Day 5/test', 'r') as f:
+  while queue:
+      start, length = queue.pop()
+      step = length // 2
+      middle = start + step
+          
+      left = computeLoc(start, maps)
+      mid = computeLoc(middle, maps)
+      right = computeLoc(start + length, maps)
+
+      minLoc = min(minLoc, left, mid, right)
+
+      if length == 1:
+        continue
+
+      if left + step != mid:
+        queue.append((start, step))
+
+      if mid + (length - step) != right:
+        queue.append((middle, length - step))
+
+  return minLoc
+
+with open('Day 5/input', 'r') as f:
   file = f.read()
   print(decipher(file))
   print(decipherEnhanced(file))
