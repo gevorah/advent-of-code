8d27c5ed581f345cf0916626325d89c8b05be239
gevorah <jhonsebastianijajiortiz@gmail.com>
2023-12-24 10:35:04 -0500

    feat: day 6 part 2

--- Day 6/6.py
+++ Day 6/6.py
@@ -8,18 +8,29 @@ def compute(paper: str):
 
   ways = []
   for time, record in zip(times, records):
-    # -hold^2 + time*hold = record
-    delta = time**2 - (-4*-record)
-    x1 = math.ceil((-time + math.sqrt(delta)) / -2)
-    x2 = math.ceil((-time - math.sqrt(delta)) / -2)
-
-    d1 = -x1**2 + time*x1
-    if d1 == record: x1 += 1
-    
-    ways.append(x2 - x1)
+    ways.append(beatIt(time, record))
   
   return  math.prod(ways)
+
+def beatIt(time, record):
+  # -hold^2 + time*hold = record
+  delta = time**2 - (-4*-record)
+  x1 = math.ceil((-time + math.sqrt(delta)) / -2)
+  x2 = math.ceil((-time - math.sqrt(delta)) / -2)
+
+  d1 = -x1**2 + time*x1
+  if d1 == record: x1 += 1
+
+  return x2 - x1
+
+def computeEnhanced(paper: str):
+  time, record = paper.split('\n')
+  time = int(re.sub(r'[^\d+]', '', time))
+  record = int(re.sub(r'[^\d+]', '', record))
+
+  return beatIt(time, record)
 
 with open('Day 6/input', 'r') as f:
   file = f.read()
   print(compute(file))
+  print(computeEnhanced(file))
