be8ceaba0918b0c75fe39d59954b15d740a8986b
gevorah <jhon.ijaji.o@gmail.com>
2024-12-01 15:10:22 -0500

    feat: day 1

--- 2024/day 1/script.py
+++ 2024/day 1/script.py
@@ -1,16 +1,25 @@
 import re
 
-def calculate(lists):
+def parse_input(file):
   regex = r'(\d+)\s+(\d+)'
-  matches = re.findall(regex, lists)
+  matches = re.findall(regex, file)
 
   left, right = zip(*matches)
   left = list(map(int, left))
   right = list(map(int, right))
-  
+
+  return left, right
+
+def calculate_distance(file):
+  left, right = parse_input(file)
   pairs = zip(sorted(left), sorted(right))
   return sum(abs(a - b) for a, b in pairs)
 
+def calculate_similarity_score(file):
+  left, right = parse_input(file)
+  return sum(x * right.count(x) for x in left)
+
 with open("input", "r") as f:
   file = f.read()
-  print(calculate(file))
+  print(calculate_distance(file))
+  print(calculate_similarity_score(file))
